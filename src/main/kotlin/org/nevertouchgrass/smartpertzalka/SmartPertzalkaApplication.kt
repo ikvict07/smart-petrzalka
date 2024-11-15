2a89a80b4534df5f575701332db3054c5d8490d0
antonhorobets <shxbb@s-mxs.net>
2024-11-15 10:46:10 +0100

    init

---
+++ src/main/kotlin/org/nevertouchgrass/smartpertzalka/SmartPertzalkaApplication.kt
@@ -0,0 +1,11 @@
+package org.nevertouchgrass.smartpertzalka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartPertzalkaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartPertzalkaApplication>(*args)
+}
---
+++ src/test/kotlin/org/nevertouchgrass/smartpertzalka/SmartPertzalkaApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.nevertouchgrass.smartpertzalka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartPertzalkaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
